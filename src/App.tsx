afa5e998f52c4cfbf5ab9e6375ac1966e9a06a8e
Owain <owain.evans@hotmail.co.uk>
2024-05-28 21:27:38 +0100

    create: modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import ResultsTable from "./components/ResultsTable";
+import logo from "./assets/cropped-1.-Logo-949-x-240-Logo-Horizontal.png";
 
 function App() {
   return (
     <>
+      <img src={logo} className="logo"></img>
       <ResultsTable />
     </>
   );
--- src/components/PlayerModal.tsx
+++ src/components/PlayerModal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { PlayerStats } from "../types";
+import closeMark from "../assets/xmark-solid.svg";
 
 interface PlayerModalProps {
   row: PlayerStats | null;
@@ -18,30 +19,32 @@ const PlayerModal: React.FC<PlayerModalProps> = ({ row, isOpen, onClose }) => {
         className="modal d-flex justify-content-center align-items-center"
         style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
       >
-        <div className="modal-content w-50">
-          <div className="modal-header">
+        <div className="modal-content w-25">
+          <div className="modal-header justify-content-between">
             <div className="d-flex flex-column flex-start align-start">
               <h5 className="modal-title">{row.firstname}</h5>
               <h6 className="text-left"> {row.surname}</h6>
             </div>
-            <button type="button" className="close" onClick={onClose}>
-              ×
-            </button>
-          </div>
-          <div className="modal-body">
-            <p>{`Bib Number: ${row.bibnumber}`}</p>
-            <p>{`Finish Time: ${row.finishtime}`}</p>
-            <p>{`Time Difference: ${row.timeDifference}`}</p>
-          </div>
-          <div className="modal-footer">
+            <div>
+              <h4>{row.flag}</h4>
+            </div>
             <button
               type="button"
-              className="btn btn-secondary"
+              className="close p-0 d-flex align-items-center justify-content-center"
               onClick={onClose}
             >
-              Close
+              <img src={closeMark} className="close-mark"></img>
             </button>
           </div>
+          <div className="modal-body d-flex gap-3">
+            <img className="player-image"></img>
+            <section className="d-flex flex-column align-items-start">
+              <p>{`Bib Number: ${row.bibnumber}`}</p>
+              <p>{`Finish Time: ${row.finishtime}`}</p>
+              <p>{`Time Difference: ${row.timeDifference}`}</p>
+            </section>
+          </div>
+          <div className="modal-footer"></div>
         </div>
       </div>
     );
